25fc05beb589b7e7b1fe66e404ba8e73314ab79a
Mario Lazzari <mario.lazzari@gmail.com>
2025-05-30 08:04:22 +0200

    Done chapter 6

---
+++ functional-react/src/App.jsx
@@ -0,0 +1,13 @@
+import { DangerButton, SuccessButton } from "./partialApplication"
+
+
+function App() {
+  return (
+		<>
+		<DangerButton text="Self Destruct!" />
+		<SuccessButton size="large" />
+		</>
+  )
+}
+
+export default App
